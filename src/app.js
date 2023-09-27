c26faf2b9c1a7382d38f75deb42c7ae72e3b7852
Peter John <peterjohngremio@gmail.com>
2023-09-27 18:18:27 -0300

    arquivo router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
+// Importar pacote do express
 const express = require('express');
+
+// Instanciar a express na variável app
 const app = express();
 
+// Importar as rotas para serem executadas na aplicação
+const router = require('./Router/crudRouter')
+
+// Importar pacote dotenv
 const dotenv = require('dotenv').config();
+
 // Setar a porta do servidor, a partir do arquivo .env
 app.set('port', process.env.PORT);
+
 // Exportar as configurações do app para outros arquivos acessrarem
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,27 +1,10 @@
 // Importar o arquivo de configuração
 const app = require('./app')
 // Importar a porta do servid
-[or
+
 const PORT = app.get('port')
 
-app.get('/api', (request, response) => {
-    response.send('Retorno de informações do banco de dados')
-    console.log('Listar informações')
-});
 
-app.post('/api', (request, response) => {
-    response.send('Método utilizado para salvar informações')
-    console.log('Listar informações')
-});
-
-app.put('/api/:id', (request, response) => {
-    response.send('Grêmio maior do sul')
-    console.log('Listar informações')
-});
-app.delete('/api/:id', (request, response) => {
-    response.send('Grêmio maior do sul')
-    console.log('Listar informações')
-});
 
 //Testar para ver se o servidor vai rodar
 app.listen(PORT, () => console.log(`Runing at the port ${PORT}`))
